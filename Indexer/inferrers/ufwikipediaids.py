a68b3e3032293782582ed9d13d3f8eb9aa860cda
Adam Bradley <adam_bradley@brown.edu>
2015-05-22 11:46:40 -0400

    Improved Wikipedia inferrence.

--- Indexer/inferrers/ufwikipediaids.py
+++ Indexer/inferrers/ufwikipediaids.py
@@ -7,7 +7,7 @@ class ufWikipediaIDs_Inferrer(Inferrer):
     def __init__(self):
         self.cachedata = None
         self.dbptempl = u'http://fragments.dbpedia.org/2014/en?subject=dbpedia:{}&predicate=dbpedia-owl:wikiPageID'
-        self.dbpheaders = {'Accept':"application/ld+json"}
+        self.dbpheaders = {'Accept':"application/json"}
         
         self.wdttempl = 'http://wikidata.dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fwikidata.dbpedia.org&query={}&format=application/json&timeout=30000'
         self.wdtquery = 'SELECT ?pid WHERE {{ <{}> dbpedia-owl:wikiPageID ?pid }}'
@@ -21,73 +21,75 @@ class ufWikipediaIDs_Inferrer(Inferrer):
         wids = []
         urls = []
         depict=[]
+        wikidata_id = False
         for source in sources: 
             href = source.get('{http://www.w3.org/1999/xlink}href')
+            
+            if href.startswith('http://wikidata.dbpedia.org/page/'):
+                href = href.replace('http://wikidata.dbpedia.org/page/', 'http://wikidata.dbpedia.org/resource/')
             
             if href.startswith('http://wikidata.dbpedia.org/resource/'):
                 sleep.append(True)
                 
                 urltempl = 'http://wikidata.dbpedia.org/sparql?default-graph-uri=http://wikidata.dbpedia.org&query=DESCRIBE+%3C{}%3E&format=application/json'
-                rsurl = 'http://wikidata.dbpedia.org/resource/Q310694'
-                url = urltempl.format(rsurl)
+                wikidata_id = href.replace('http://wikidata.dbpedia.org/resource/', '')
+                url = urltempl.format(href)
                 
                 data = requests.get(url)
                 data = data.json()
                 
-                for k, v in data.items():
-                	if k == rsurl:
-                	    if 'http://www.w3.org/2000/01/rdf-schema#seeAlso' in v:
-                	        for sa in v['http://www.w3.org/2000/01/rdf-schema#seeAlso']:
-                	            urls.append( sa['value'] )
-                	    
-                	    if 'http://xmlns.com/foaf/0.1/depiction' in v:
-                	        for sa in v['http://xmlns.com/foaf/0.1/depiction']:
-                	            depict.append( sa['value'] )
-                	    
-                	    if 'http://www.w3.org/2002/07/owl#sameAs' in v:
-                	        for sa in v['http://www.w3.org/2002/07/owl#sameAs']:
-                	            if sa['value'].startswith('http://dbpedia.org/resource/'):
-                	                href = sa['value'].replace('http://dbpedia.org/resource/', 'http://dbpedia.org/page/')
+                if href in data:
+                    v = data[href]
+            	    if 'http://www.w3.org/2000/01/rdf-schema#seeAlso' in v:
+            	        for sa in v['http://www.w3.org/2000/01/rdf-schema#seeAlso']:
+            	            urls.append( sa['value'] )
+            	    
+            	    if 'http://xmlns.com/foaf/0.1/depiction' in v:
+            	        for sa in v['http://xmlns.com/foaf/0.1/depiction']:
+            	            depict.append( sa['value'] )
+            	    
+            	    if 'http://www.w3.org/2002/07/owl#sameAs' in v:
+            	        for sa in v['http://www.w3.org/2002/07/owl#sameAs']:
+            	            if sa['value'].startswith('http://dbpedia.org/resource/'):
+            	                href = sa['value'].replace('http://dbpedia.org/resource/', 'http://dbpedia.org/page/')
             
             if href.startswith('http://dbpedia.org/page/'):
                 sleep.append(True)
-                dbname = href.replace('http://dbpedia.org/page/', '')
-                url = self.dbptempl.format(dbname)
+                url = href.replace('http://dbpedia.org/page/', 'http://dbpedia.org/data/')+'.json'
+                resname = href.replace('http://dbpedia.org/page/', 'http://dbpedia.org/resource/')
                 try:
                     resp = requests.get(url, headers=self.dbpheaders)
-                    wid = resp.json()['@graph'][0]['dbpedia-owl:wikiPageID']
-                    if wid:
-                        wids.append(int(wid))
+                    data = resp.json()[resname]
+                    if "http://dbpedia.org/ontology/wikiPageExternalLink" in data:
+                        for url in data["http://dbpedia.org/ontology/wikiPageExternalLink"]:
+                            urls.append(url['value'])
+                    if (not wikidata_id) and ("http://www.w3.org/2002/07/owl#sameAs" in data):
+                        for i in data["http://www.w3.org/2002/07/owl#sameAs"]:
+                            if i['value'].startswith('http://wikidata.dbpedia.org/resource/'):
+                                wikidata_id = i['value'].replace('http://wikidata.dbpedia.org/resource/', '')
+                    if "http://xmlns.com/foaf/0.1/depiction" in data:
+                        for i in data["http://xmlns.com/foaf/0.1/depiction"]:
+                            depict.append(i['value'])
+                    if "http://dbpedia.org/ontology/wikiPageID" in data:
+                        for i in data["http://dbpedia.org/ontology/wikiPageID"]:
+                            wids.append(i['value'])
+                    
                 except Exception as e:
                     pass
                     
             #http://wikidata.dbpedia.org/sparql?default-graph-uri=http://wikidata.dbpedia.org&query=DESCRIBE+%3Chttp://wikidata.dbpedia.org/resource/Q310694%3E&format=application/json
        
+        #TODO: Pull each URL, discard 404s, record titles.
+       
         outp = {}
        
         if wids:
-            outp['wiki_id'] = wids 
+            outp['wiki_id'] = list(set(wids))
         if urls:
-            outp['inferred_url'] = urls
+            outp['inferred_url'] = list(set(urls))
         if depict:
-            outp['inferred_depiction'] = depict
+            outp['inferred_depiction'] = list(set(depict))
+        if wikidata_id:
+            outp['wikidata_id'] = str(wikidata_id)
         
         return outp
-            
-    def append(self, inferred, xml):
-        if inferred:
-            root = xml.getroot()
-            doc = root.getchildren()[0]
-            
-            #Legacy workaround. TODO: Remove.
-            if 'wids' in inferred:
-                wikiIds = inferred['wids']
-            elif 'wiki_id' in inferred:
-                wikiIds = inferred['wiki_id']
-            else:
-                return
-            
-            for wid in wikiIds:
-                newadd = etree.Element('field', name='wiki_id')
-                newadd.text = str(wid)
-                doc.append(newadd)
